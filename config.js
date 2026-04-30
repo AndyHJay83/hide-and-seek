889eb2317acb67ddde38afbd7c7022e125039dc7
AndyHJay83 <120287095+AndyHJay83@users.noreply.github.com>
2026-04-30 12:49:36 +0100

    Updated config.js with new domain

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 const SUPABASE_URL = 'https://eywsdoevxpelkfporvkf.supabase.co';
 const SUPABASE_ANON_KEY = 'sb_publishable_nhdKmL-I0YmaSiUbCSWZyw_13T0iN3l';
-const APP_URL = 'https://andyhjay83.github.io/hide-and-seek/';
+const APP_URL = 'https://hide-and-seek-eta.vercel.app';
